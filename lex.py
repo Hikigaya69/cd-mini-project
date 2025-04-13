63f667cc457d9577a26e01340299f6661a3ade55
Yashas Hegde <yashubugadi@gmail.com>
2025-04-13 19:55:56 +0530

    Update lex.py

--- lex.py
+++ lex.py
@@ -1,5 +1,5 @@
 import re
-from collections import defaultdict, deque
+from collections import defaultdict
 
 # === LEXER PART === #
 
@@ -55,80 +55,3 @@ with open("token_stream.txt", "w", encoding="utf-8") as f:
     for kind, val in tokens:
         if kind in ["KEYWORD", "EXPR", "RELOP", "IDENTIFIER", "PUNCTUATION"]:
             f.write(f"{kind}\n")
-
-# === GRAMMAR & FIRST/FOLLOW PART === #
-
-# Grammar definition based on the sample input
-grammar = {
-    "PROGRAM":      [["main_block"]],
-    "main_block":   [["KEYWORD", "KEYWORD", "DECLS", "STMTS", "KEYWORD"]],
-    "DECLS":        [["KEYWORD", "ID_LIST", "PUNCTUATION"]],
-    "ID_LIST":      [["IDENTIFIER", "ID_TAIL"]],
-    "ID_TAIL":      [["PUNCTUATION", "ID_LIST"], []],
-    "STMTS":        [["STMT", "STMTS"], []],
-    "STMT":         [["KEYWORD", "PUNCTUATION", "EXPR", "RELOP", "EXPR", "PUNCTUATION", "KEYWORD", "ACTION", "KEYWORD"]],
-    "ACTION":       [["KEYWORD", "PUNCTUATION", "IDENTIFIER", "PUNCTUATION"]],
-}
-
-non_terminals = set(grammar.keys())
-first = defaultdict(set)
-follow = defaultdict(set)
-
-# Compute FIRST sets
-def compute_first(symbol):
-    if symbol not in non_terminals:
-        return {symbol}
-
-    if first[symbol]:
-        return first[symbol]
-
-    for prod in grammar[symbol]:
-        if not prod:
-            first[symbol].add("ε")
-        else:
-            for sym in prod:
-                sym_first = compute_first(sym)
-                first[symbol] |= (sym_first - {"ε"})
-                if "ε" not in sym_first:
-                    break
-            else:
-                first[symbol].add("ε")
-    return first[symbol]
-
-# Compute FOLLOW sets
-def compute_follow():
-    follow["PROGRAM"].add("$")
-    changed = True
-    while changed:
-        changed = False
-        for nt in grammar:
-            for prod in grammar[nt]:
-                trailer = follow[nt].copy()
-                for symbol in reversed(prod):
-                    if symbol in non_terminals:
-                        if follow[symbol] != follow[symbol] | trailer:
-                            follow[symbol] |= trailer
-                            changed = True
-                        if "ε" in first[symbol]:
-                            trailer |= (first[symbol] - {"ε"})
-                        else:
-                            trailer = first[symbol]
-                    else:
-                        trailer = compute_first(symbol)
-
-# Run FIRST/FOLLOW computations
-for non_term in grammar:
-    compute_first(non_term)
-compute_follow()
-
-# Write FIRST sets to file
-with open("first.txt", "w", encoding="utf-8") as ff:
-    ff.write("FIRST Sets:\n" + "-" * 40 + "\n")
-    for nt in grammar:
-        ff.write(f"{nt}: {', '.join(first[nt])}\n")
-
-# Write FOLLOW sets to file
-with open("follow.txt", "w", encoding="utf-8") as ffw:
-    ffw.write("FOLLOW Sets:\n" + "-" * 40 + "\n")
-    for nt in grammar:
-        ffw.write(f"{nt}: {', '.join(follow[nt])}\n")
